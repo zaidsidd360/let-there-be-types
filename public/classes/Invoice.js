1682c7b4f45f020be479a0cd40718a6dde182884
zaidsidd360 <zaidsidd360@gmail.com>
2022-11-06 05:01:43 +0530

    for deployment

--- public/classes/Invoice.js
+++ public/classes/Invoice.js
@@ -1,10 +1,11 @@
 export class Invoice {
-    constructor(client, details, amount) {
-        this.client = client;
-        this.details = details;
-        this.amount = amount;
-    }
-    format() {
-        return `${this.client} owes $${this.amount} for ${this.details}`;
-    }
+  constructor(client, details, amount) {
+    this.client = client;
+    this.details = details;
+    this.amount = amount;
+  }
+
+  format() {
+    return `${this.client} owes $${this.amount} for ${this.details}`;
+  }
 }
--- public/classes/Payment.js
+++ public/classes/Payment.js
@@ -1,10 +1,11 @@
 export class Payment {
-    constructor(recepient, details, amount) {
-        this.recepient = recepient;
-        this.details = details;
-        this.amount = amount;
-    }
-    format() {
-        return `${this.recepient} is owed $${this.amount} for ${this.details}`;
-    }
+  constructor(recepient, details, amount) {
+    this.recepient = recepient;
+    this.details = details;
+    this.amount = amount;
+  }
+
+  format() {
+    return `${this.recepient} is owed $${this.amount} for ${this.details}`;
+  }
 }
